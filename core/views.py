93223386577ac61864091bd61fe263f221d509a1
@rcaterino <rcaterino.dev@gmail.com>
2023-05-15 15:42:29 +0200

    InittialCommit+startproject+apps core & pages

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from typing import Any
+from django import http
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':"Mi Super web playground"})
+    
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
